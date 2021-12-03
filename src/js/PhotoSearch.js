f676106d8eaf40b881be0511922e6d7c21dd95fd
Valdoveste <caio.valdoveste@outlook.com.br>
2021-12-02 22:01:56 -0300

    Pagination & Search

---
+++ src/js/PhotoSearch.js
@@ -0,0 +1,16 @@
+import { perPageLimit, pictureContainer } from "./Album.js";
+import { generatePictureHTML } from './Album.js';
+import getPost  from './Album.js';
+
+export default function searchPhotos(subject){
+    // console.log("page: ", index);
+    let pageNumber = document.getElementById('page-number').placeholder = 1;
+    let baseURL = `https://api.pexels.com/v1/search?query=${subject}&page=1&per_page=${perPageLimit}`;
+    
+    getPost(baseURL).then(resp => {
+        return resp.json()
+    }).then(data => {
+        pictureContainer.innerHTML = "";
+        generatePictureHTML(data.photos);
+    })
+}
